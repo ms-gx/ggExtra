5d6091c0c38c514b62786f8d491e625201320fce
Michael Schmid <45788632+ms-gx@users.noreply.github.com>
2021-07-03 23:48:07 +0200

    Update ggMarginal.R

--- R/ggMarginal.R
+++ R/ggMarginal.R
@@ -206,7 +206,8 @@ print.ggExtraPlot <- function(x, newpage = grDevices::dev.interactive(), ...) {
     x <- ggplot2::ggplot() +
       ggplot2::geom_blank() +
       ggplot2::annotation_custom(x) +
-      ggplot2::theme_void()
+      ggplot2::theme_void() +
+      ggplot2::scale_y_continuous()
     print(x)
   } else {
     grid::grid.draw(x)
